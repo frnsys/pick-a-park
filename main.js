66a5f4f6c3d791b3dca88120bed4c2a1bdadb9dd
Francis Tseng <f+accounts@frnsys.com>
2024-06-16 18:43:35 +0200

    global version launch

--- main.js
+++ main.js
@@ -1,28 +1,43 @@
+// - Handle multiple search matches
+
 const OSM_API_URL = 'https://overpass-api.de/api/interpreter';
 const BBOX_SIZE = 0.005;
-const BOROUGH_IDS = {
-  'bronx': '3609691916',
-  'queens': '3609691819',
-  'brooklyn': '3609691750',
-  'manhattan': '3608398124',
-  'staten_island': '3609691948'
-};
-const BOROUGH_PARKS = {};
 const PICK_BUTTON = document.getElementById('select-park');
+const ERROR = document.getElementById('error-message');
+const PARKS = {};
 
+function queryArea(query, cb) {
+  const nominatimUrl = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(query)}&format=json&polygon_geojson=1`;
+  fetch(nominatimUrl, {
+    headers: {
+      'Accept': 'application/json',
+    },
+    method: 'GET',
+  })
+    .then(res => res.json())
+    .then(matches => {
+      if (matches.length > 0) {
+        // See <https://giswiki.hsr.ch/Overpass_API>
+        const osmId = matches[0].osm_id;
+        const areaId = 3600000000 + osmId;
+        cb(areaId);
+      } else {
+        cb(null);
+      }
+    });
+}
 
-function loadBoroughParks(borough, cb) {
-  const boroughId = BOROUGH_IDS[borough];
-
-  // Could have
-  //  way["leisure"="park"](area.searchArea);
-  //  relation["leisure"="park"](area.searchArea);
-  // in addition to nodes, but only nodes have lat/lons
+function loadParks(areaId, cb) {
+  // I think we can leave out relations as they contain ways that would
+  // already be labeled as parks.
+  //
+  // relation["leisure"="park"](area.searchArea);
   const osmQuery = `
     [out:json][timeout:25];
-    area(${boroughId})->.searchArea;
+    area(${areaId})->.searchArea;
     (
       node["leisure"="park"](area.searchArea);
+      way["leisure"="park"](area.searchArea);
     );
     out body;
     >;
@@ -40,34 +55,82 @@ function loadBoroughParks(borough, cb) {
   })
     .then(res => res.json())
     .then(({elements}) => {
-      const parks = elements.filter((el) => el['tags'] && el['tags']['name']);
-      cb(parks);
+      let nodes = {};
+      let parks = [];
+      elements.forEach((el) => {
+        switch (el.type) {
+          case "node":
+            nodes[el.id] = el;
+            break;
+
+          case "way":
+            if (el['tags'] && el['tags']['name']) {
+              parks.push(el);
+            }
+            break;
+
+          case "relation":
+            break;
+        }
+      });
+      cb({parks, nodes});
     })
     .catch(err => {
       throw err;
     });
 }
 
+function rand(arr) {
+  let idx = Math.floor(Math.random() * arr.length);
+  return arr[idx];
+}
 
 function pickRandomPark() {
   PICK_BUTTON.disabled = true;
-  const borough = document.getElementById('select-borough').value;
-  if (!(borough in BOROUGH_PARKS)) {
-    loadBoroughParks(borough, (parks) => {
-      BOROUGH_PARKS[borough] = parks;
-      pickRandomPark();
+  PICK_BUTTON.innerText = "Searching for parks...";
+  const query = document.getElementById('area-query').value;
+  if (query.length == 0) {
+    return;
+  }
+
+  if (!(query in PARKS)) {
+    queryArea(query, (areaId) => {
+      if (areaId == null) {
+        ERROR.innerText = "No places found";
+        ERROR.style.display = "block";
+        PICK_BUTTON.disabled = false;
+        PICK_BUTTON.innerText = "Pick a Park";
+      } else {
+        loadParks(areaId, (results) => {
+          PARKS[query] = results;
+          pickRandomPark();
+        });
+      }
     });
   } else {
-    let parks = BOROUGH_PARKS[borough];
-    let idx = Math.floor(Math.random() * parks.length);
-    let park = parks[idx];
-    document.getElementById('selected-park').innerText = park['tags']['name'];
-    document.getElementById('selected-park-map').src = `https://www.openstreetmap.org/export/embed.html?bbox=${park['lon']-BBOX_SIZE}%2C${park['lat']-BBOX_SIZE}%2C${park['lon']+BBOX_SIZE}%2C${park['lat']+BBOX_SIZE}&layer=mapnik&marker=${park['lat']}%2C${park['lon']}`;;
-    document.getElementById('selected-park-street-view').href = `https://maps.google.com/maps?q=&layer=c&cbll=${park['lat']},${park['lon']}&cbp=11,0,0,0,0`;
-    document.getElementById('selected-park-info').style.display = 'block';
+    let {parks, nodes} = PARKS[query];
+    if (parks.length == 0) {
+      ERROR.innerText = "No parks found";
+      ERROR.style.display = "block";
+    } else {
+      ERROR.style.display = "none";
+      ERROR.innerText = "";
+      let park = rand(parks);
+      let node = nodes[rand(park.nodes)];
+      document.getElementById('selected-park').innerText = park['tags']['name'];
+      document.getElementById('selected-park-map').src = `https://www.openstreetmap.org/export/embed.html?bbox=${node['lon']-BBOX_SIZE}%2C${node['lat']-BBOX_SIZE}%2C${node['lon']+BBOX_SIZE}%2C${node['lat']+BBOX_SIZE}&layer=mapnik&marker=${node['lat']}%2C${node['lon']}`;;
+      document.getElementById('selected-park-street-view').href = `https://maps.google.com/maps?q=&layer=c&cbll=${node['lat']},${node['lon']}&cbp=11,0,0,0,0`;
+      document.getElementById('selected-park-info').style.display = 'block';
+    }
     PICK_BUTTON.disabled = false;
+    PICK_BUTTON.innerText = "⟲ Pick Another Park";
   }
 }
 
+// Submit on enter
+document.getElementById('area-query').addEventListener('keydown', (ev) => {
+  if (ev.key === 'Enter') {
+    pickRandomPark();
+  }
+});
 PICK_BUTTON.addEventListener('click', pickRandomPark);
-
